8c77711db512e5af7bd799c303dcb1655816cd3f
sickerin <c.danpm@gmail.com>
2016-12-17 16:07:22 +0000

    Elimated redudancy and prepare for production

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+   before_action :set_article, only: [:edit, :update, :show, :destroy]
    
     def index
         @articles = Article.all
@@ -14,7 +15,6 @@ class ArticlesController < ApplicationController
     
     def create
         #render plain: params[:article].inspect
-        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Your article was successfully created!"
             redirect_to articles_path(@article)
@@ -25,7 +25,6 @@ class ArticlesController < ApplicationController
     
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was succesfully updated"
             redirect_to article_path(@article)
@@ -35,17 +34,19 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article has been destroyed"
         redirect_to articles_path
     end
     
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
         params.require(:article).permit(:title, :description)
     
